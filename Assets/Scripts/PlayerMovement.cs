095b1a6bbbc1b26aff1785f77cd967b70c052a4e
Arthur So <soarthur@hotmail.com>
2021-11-02 17:27:16 -0300

    Adicionar rotação do mouse para o jogador

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,30 +8,45 @@ public class PlayerMovement : MonoBehaviour
     public float movementSpeed;
     public Camera cam;
 
-    Vector3 movement;
+    Vector2 movement;
+    // Vector3 mousePos;
 
     void FixedUpdate()
     {
+        MovePlayer();
+        RotatePlayer();
+    }
+
+    void RotatePlayer(){
+        Plane plane = new Plane(Vector3.up, 0f);
+        Ray ray = cam.ScreenPointToRay( Input.mousePosition );
+        float distanceToPlane;
+
+        if( plane.Raycast( ray , out distanceToPlane )){
+            Vector3 lookDir = ray.GetPoint(distanceToPlane);
+            lookDir -= rb.position;
+
+            float angle = -Mathf.Atan2( lookDir.z , lookDir.x ) * Mathf.Rad2Deg;
+            rb.rotation = Quaternion.Euler( new Vector3( 0,angle,0 ) );
+        }
+    }
+
+    void MovePlayer(){
         movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = 0;
-        movement.z = Input.GetAxisRaw("Vertical");
+        movement.y = Input.GetAxisRaw("Vertical");
 
         Vector3 forward = cam.transform.forward;
         Vector3 right = cam.transform.right;
-        
+
         forward.y = 0;
         forward.Normalize();
-        
+
         right.y = 0;
         right.Normalize();
 
-        forward *=  movement.z;
+        forward *= movement.y;
         right *= movement.x;
-        
-        print ( right );
 
-        movement = forward + right;
-
-        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + (forward + right) * movementSpeed * Time.fixedDeltaTime);
     }
 }
