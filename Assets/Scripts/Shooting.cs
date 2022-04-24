361bf4df2eda69f608b11a7da714038c7f522e1d
Arthur R P So <soarthur@hotmail.com>
2022-04-24 17:46:34 -0300

    Adicionar cooldown ao script shooting

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -7,20 +7,28 @@ public class Shooting : MonoBehaviour
     public Transform firePoint;
     public GameObject bulletPrefab;
 
+    public float shotCooldown = 0.5f;
     public float bulletForce = 20f;
 
-    // Update is called once per frame
+    private float cooldown = 0.0f;
+
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (cooldown > 0)
+        {
+            cooldown -= Time.fixedDeltaTime;
+        }
+ 
+        if (Input.GetButtonDown("Fire1") && cooldown <= 0)
         {
             Shoot();
+            cooldown += shotCooldown;
         }
     }
 
     void Shoot()
     {
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation * Quaternion.Euler( 0,90,0 ));
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation * Quaternion.Euler(0, 90, 0));
         Rigidbody rb = bullet.GetComponent<Rigidbody>();
         rb.AddForce(firePoint.forward * bulletForce, ForceMode.Impulse);
     }
